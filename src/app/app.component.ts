ae71df56ad42ec5f38477c978ec1deb30fb87e9d
njagadeesan <nandhakumar.jagadeesan@nextsphere.com>
2020-06-09 03:37:02 +0530

    minor commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = "Angular 5";
